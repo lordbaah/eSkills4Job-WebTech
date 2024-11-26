f8d0822dc4a3ff8012598dc16dcb2eb323373476
lordbaah <79156645+lordbaah@users.noreply.github.com>
2024-11-26 13:20:54 +0000

    commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,6 +6,14 @@ const express = require("express");
 const studentsRoutes = require("./routes/studentsRoutes");
 const db = require("./database");
 
+const sequelize = require("./database");
+
+// Import your model
+const Student = require("./models/students");
+const Department = require("./models/department");
+const Course = require("./models/courses");
+const Enrollment = require("./models/enrollment");
+
 // db.execute("SELECT * FROM student")
 //   .then((result) => {
 //     console.log(result);
@@ -15,22 +23,71 @@ const db = require("./database");
 //   });
 
 // Test database connection
-db.authenticate()
+// db.authenticate()
+//   .then(() => {
+//     console.log("Database connected successfully.");
+//   })
+//   .catch((error) => {
+//     console.error("Unable to connect to the database:", error);
+//   });
+
+// // Execute a raw SQL query using Sequelize
+// db.query("SELECT * FROM students", { type: db.QueryTypes.SELECT })
+//   .then((students) => {
+//     console.log("Query result:", students);
+//   })
+//   .catch((error) => {
+//     console.error("Query error:", error);
+//   });
+
+//authenticating database connection
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Database connected successfully.");
+//     return sequelize.sync({ force: true }); // Force recreates all tables
+//   })
+//   .then(() => {
+//     console.log("Tables recreated successfully.");
+//   })
+//   .catch((error) => {
+//     console.error("Error setting up database:", error);
+//   });
+
+// creating database with sequilize
+sequelize
+  .sync()
+  // .sync({ force: true })
+  // .sync({ alter: true }) // Alters the table to match the new model
   .then(() => {
-    console.log("Database connected successfully.");
+    console.log("Tables recreated successfully.");
+    // return Student.create({
+    //   studentName: "John Doe",
+    //   dob: "2000-01-01",
+    // });
   })
+  // .then((student) => {
+  //   console.log("Sample student created:", student);
+  // })
   .catch((error) => {
-    console.error("Unable to connect to the database:", error);
+    console.error("Error:", error);
   });
 
-// Execute a raw SQL query using Sequelize
-db.query("SELECT * FROM students", { type: db.QueryTypes.SELECT })
-  .then((students) => {
-    console.log("Query result:", students);
-  })
-  .catch((error) => {
-    console.error("Query error:", error);
-  });
+// setting association
+const models = {
+  Student,
+  Course,
+  Department,
+  Enrollment,
+};
+
+Object.keys(models).forEach((modelName) => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+module.exports = models;
 
 const server = express();
 
--- nodejs/models/students.js
+++ nodejs/models/students.js
@@ -1,21 +1,35 @@
-const Sequelize = require("sequelize");
-
 // import db = database connection
 const db = require("../database");
 
-const Student = db.define("students", {
+const Sequelize = require("sequelize");
+// const sequelize = require("../database"); // Path to your database file
+
+const Student = db.define("student", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     primaryKey: true,
     allowNull: false,
   },
-  studentName: {
+  firstName: {
     type: Sequelize.STRING,
     allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true, // Ensures the value is a valid email address
+    },
   },
   dob: {
     type: Sequelize.DATE,
     allowNull: false,
   },
 });
+
+module.exports = Student;
